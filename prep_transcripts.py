0248ddecebfc186371357e6708d5577684d1b94e
Dorothea <dorothea.french@gmail.com>
2025-07-25 13:33:39 -0600

    update test

--- prep_transcripts.py
+++ prep_transcripts.py
@@ -1,17 +1,17 @@
 import pandas as pd
 
-file = "C:\\Users\\Dorot\\Emotive Computing Dropbox\\Dorothea French\\ASR_error_correction\\data\\FINAL LoFi WITH SCORES NO MATCHING -LoFi_With_Propensity_Scores_4_11.xlsx"
+file = "C:\\Users\\Dorot\\Emotive Computing Dropbox\\Dorothea French\\ASR_error_correction\\data\\LoFi+whisper-large-v2_utterances_demog.xlsx"
 
 all_transcripts = pd.read_excel(file)
 
 print(all_transcripts.columns)
 
-all_transcripts = all_transcripts[["utterance_in_recording", "recordingID", "speaker", "transcript", "ASR", "confidence", "Ethnicity", "Race Description", "Gender"]]
+all_transcripts = all_transcripts[["utterance_in_recording", "recordingID", "speaker", "transcript", "ASR", "confidence", "Ethnicity", "Race Description", "Personal Pronouns"]]
 
 transcript_groups = all_transcripts.groupby("recordingID")
 
 pd.options.display.max_colwidth = 200
 
 for ID, group in transcript_groups:
-    print(group["speaker"].head(10) + ": " + group["ASR"].head(10))
+    print(group["speaker"].head(10).values + ": " + group["ASR"].head(10).values)
     # group.to_excel("C:\\Users\\Dorot\\Emotive Computing Dropbox\\Dorothea French\\ASR_error_correction\\data\\transcripts\\"+ ID +".xlsx")
