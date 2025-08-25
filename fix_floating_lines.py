a5a24629ce8aede7c8ca0e79701a4965525a15d8
Dorothea <dorothea.french@gmail.com>
2025-08-25 10:19:53 -0600

    train test 2

--- fix_floating_lines.py
+++ fix_floating_lines.py
@@ -10,13 +10,11 @@ def cycle_through_transcripts(location):
     i = 0
     for file in transcripts:
         transcript = pd.read_excel(file)
-        if 'Unnamed: 0.1' in transcript.columns:
-            i += 1
-            shutil.move(file, file.replace("reasoning_generated/", "reasoning_generated/failed/"))
-    print(i)
+        if "okay three over three that's once plus three over three" in transcript['ASR'].values:
+            print(file)
 
 
 
-location = "/mnt/c/Users/Dorot/Emotive Computing Dropbox/Dorothea French/ASR_error_correction/data/reasoning_generated/"
+location = "/mnt/c/Users/Dorot/Emotive Computing Dropbox/Dorothea French/ASR_error_correction/data/transcripts/"
 
 cycle_through_transcripts(location)
